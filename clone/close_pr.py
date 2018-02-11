260ef5e1e39c61606634dbf73705137d42904d5b
Mariatta Wijaya <mariatta@python.org>
2018-02-11 14:27:21 -0800

    Test out things.

--- clone/close_pr.py
+++ clone/close_pr.py
@@ -1,11 +1,27 @@
 """Automatically close a PR as it opens."""
 
 import gidgethub.routing
-from . import tasks
 router = gidgethub.routing.Router()
 
 
 @router.register("pull_request", action="opened")
 @router.register("pull_request", action="reopened")
 async def close_pr(event, gh, *args, **kwargs):
-    tasks.close_pr.delay(event, gh)
+    close_the_pr(event, gh)
+
+
+def close_the_pr(event, gh):
+    data = {'state': 'closed',
+            'maintainer_can_modify': True}
+    gh.patch(event.data["pull_request"]["url"], data=data)
+    pr_comment = {
+        'body': "Close it. I'm a bot."
+    }
+
+    print("getitem")
+    print(pr_comment)
+
+    for item in gh.getiter('/repos/mariatta/cpython/git/refs/heads'):
+        print(item)
+
+    gh.post(event.data["pull_request"]["comments_url"], data=pr_comment)
